d8399a9385cea543931b3c51e7840bf947e35ba5
kislypolly <kislypolly06@gmail.com>
2025-12-23 20:19:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './hooks/useAuth'
 import App from './App.tsx'
 import { store } from './store'
 import './index.css'
@@ -10,11 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AuthProvider> 
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
