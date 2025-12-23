869b6164ede6aceae4f906393fb69df682c16388
kislypolly <kislypolly06@gmail.com>
2025-12-23 18:30:37 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import HomePage from './pages/HomePage'
+import RecipePage from './pages/RecipePage'
+import AuthPage from './pages/AuthPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
+        <Route path="recipe/:id" element={<RecipePage />} />
+        <Route path="auth" element={<AuthPage />} />
       </Route>
     </Routes>
   )
 }
+
 export default App
