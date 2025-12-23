9be49aded42968f3624e52c7736cc9dee5f1dd4e
kislypolly <kislypolly06@gmail.com>
2025-12-23 23:34:56 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { Provider, useSelector } from 'react-redux';
-import { store, type RootState } from './store';
-import Layout from './components/Layout';
-import HomePage from './pages/HomePage';
-import RecipePage from './pages/RecipePage';
-import AuthPage from './pages/AuthPage';
-import CreateRecipePage from './pages/CreateRecipePage';
-import ProfilePage from './pages/ProfilePage';
-
-const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const user = useSelector((state: RootState) => state.auth?.user);
-  return user ? children : <Navigate to="/auth?mode=login" />;
-};
-
-const AppContent = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<HomePage />} />
-      <Route path="recipe/:id" element={<RecipePage />} />
-      <Route path="auth" element={<AuthPage />} />
-      <Route path="create" element={<ProtectedRoute><CreateRecipePage /></ProtectedRoute>} />
-      <Route path="profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-      <Route path="edit/:id" element={<ProtectedRoute><CreateRecipePage /></ProtectedRoute>} />
-    </Route>
-    <Route path="*" element={<Navigate to="/" />} />
-  </Routes>
-);
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import HomePage from './pages/HomePage'
+import RecipePage from './pages/RecipePage'
+import AuthPage from './pages/AuthPage'
+import CreateRecipePage from './pages/CreateRecipePage'
+import ProfilePage from './pages/ProfilePage'
 
 function App() {
   return (
-    <Provider store={store}>
-      <AppContent />
-    </Provider>
-  );
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="recipe/:id" element={<RecipePage />} />
+        <Route path="auth" element={<AuthPage />} />
+        <Route path="create" element={<CreateRecipePage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="edit/:id" element={<CreateRecipePage />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
