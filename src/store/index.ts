740a7ce3b30f5431f6cdd964b825386068b3460f
kislypolly <kislypolly06@gmail.com>
2025-12-23 13:05:40 +0300

    Create index.ts

---
+++ src/store/index.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import recipeSlice from './recipeSlice'
+
+export const store = configureStore({
+  reducer: {
+    recipes: recipeSlice,
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
