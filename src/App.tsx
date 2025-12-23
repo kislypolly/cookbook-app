464c40bbec4cd7ade55e2f035364b16f3bfcb006
kislypolly <kislypolly06@gmail.com>
2025-12-23 23:15:17 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import type { RootState } from './store'; 
+import { Provider, useSelector } from 'react-redux';
+import { store, type RootState } from './store';
 import Layout from './components/Layout';
 import HomePage from './pages/HomePage';
 import RecipePage from './pages/RecipePage';
@@ -8,48 +8,30 @@ import AuthPage from './pages/AuthPage';
 import CreateRecipePage from './pages/CreateRecipePage';
 import ProfilePage from './pages/ProfilePage';
 
-function App() {
-  const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-    const user = useSelector((state: RootState) => state.auth?.user);
-    return user ? children : <Navigate to="/auth?mode=login" />;
-  };
+const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const user = useSelector((state: RootState) => state.auth?.user);
+  return user ? children : <Navigate to="/auth?mode=login" />;
+};
 
+const AppContent = () => (
+  <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<HomePage />} />
+      <Route path="recipe/:id" element={<RecipePage />} />
+      <Route path="auth" element={<AuthPage />} />
+      <Route path="create" element={<ProtectedRoute><CreateRecipePage /></ProtectedRoute>} />
+      <Route path="profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
+      <Route path="edit/:id" element={<ProtectedRoute><CreateRecipePage /></ProtectedRoute>} />
+    </Route>
+    <Route path="*" element={<Navigate to="/" />} />
+  </Routes>
+);
+
+function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Публичные страницы */}
-        <Route index element={<HomePage />} />
-        <Route path="recipe/:id" element={<RecipePage />} />
-        <Route path="auth" element={<AuthPage />} />
-        
-        {/* Защищенные страницы */}
-        <Route 
-          path="create" 
-          element={
-            <ProtectedRoute>
-              <CreateRecipePage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="profile" 
-          element={
-            <ProtectedRoute>
-              <ProfilePage />
-            </ProtectedRoute>
-          } 
-        />
-        <Route 
-          path="edit/:id" 
-          element={
-            <ProtectedRoute>
-              <CreateRecipePage />
-            </ProtectedRoute>
-          } 
-        />
-      </Route>
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
   );
 }
 
