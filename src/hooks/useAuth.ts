fc87b552b87b087c8a51547f3f75a2fe7cc14a95
kislypolly <kislypolly06@gmail.com>
2025-12-23 20:35:26 +0300

    Update useAuth.ts

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -23,20 +23,27 @@ export const useAuth = () => {
   }, [])
 
   const signUp = async (email: string, password: string) => {  
+    const cleanEmail = email.trim().toLowerCase()
+    console.log('Signup:', cleanEmail)
+    
     const { data, error } = await supabase.auth.signUp({
       email: cleanEmail,
-    password: password 
+      password: password 
     })
-    console.log('✅ Result:', data, error)
-  if (error) throw new Error(error.message)
+    console.log('Signup Result:', data, error)
+    if (error) throw new Error(error.message)
   }
   
   const signIn = async (email: string, password: string) => {
+    const cleanEmail = email.trim().toLowerCase()
+    console.log('Login:', cleanEmail)
+    
     const { data, error } = await supabase.auth.signInWithPassword({
-      email,
+      email: cleanEmail,
       password
     })
-    if (error) throw error
+    console.log('Login Result:', data, error)
+    if (error) throw new Error(error.message)
   }
 
   const signOut = async () => {
