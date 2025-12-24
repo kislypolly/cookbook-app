441efb99fb0ac1d76a751f9e71bd862fd0ae2980
kislypolly <kislypolly06@gmail.com>
2025-12-24 04:53:05 +0300

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { recipeApi } from './api'
+import authReducer from './authSlice'
+import recipeReducer from './recipeSlice'
 
 export const store = configureStore({
-  reducer: { [recipeApi.reducerPath]: recipeApi.reducer },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(recipeApi.middleware),
+  reducer: {
+    auth: authReducer,
+    recipes: recipeReducer,
+    [recipeApi.reducerPath]: recipeApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(recipeApi.middleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
